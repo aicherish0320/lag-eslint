27ec4746c4d039e24d1521bed7182bbfea564c85
aicherish0320 <aicherish0320@163.com>
2022-05-27 10:56:27 +0800

    eslint 结合 wwebpack 其他配置

--- zce-react-app/.eslintrc.js
+++ zce-react-app/.eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   rules: {
+    'space-before-function-paren': 0
+    // 'react/jsx-uses-react': 2,
+    // 'react/jsx-uses-vars': 2
   }
+  // plugins: ['react']
 }
--- zce-react-app/src/components/App.js
+++ zce-react-app/src/components/App.js
@@ -3,10 +3,17 @@ import './App.css'
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           Learn React
         </a>
       </header>
